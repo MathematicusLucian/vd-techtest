aba1269228ed1a5a3638188ba9f24149f664e508
Lucius Mathametica <52294958+k1n4kut4@users.noreply.github.com>
2019-07-02 20:01:40 +0100

    Code for first button click... grab colour from select element

---
+++ js/script.js
@@ -0,0 +1,13 @@
+$( document ).ready(function() {
+
+	$("#btn-product_color").click(function(e){
+ 
+		e.preventDefault();
+
+	    var colour = $("#select_product-colour").val(); 
+
+	    return false;
+
+	});
+
+});
